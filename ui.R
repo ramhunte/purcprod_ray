e050a4c1768768e07ec423ec28d392dc8e58e6e7
ramhunte <ramhunte@ucsb.edu>
2025-04-07 21:30:07 -0700

    NOAA header in navbar

--- ui.R
+++ ui.R
@@ -19,6 +19,40 @@ shinyUI(
     page_navbar(
       id = "navbar",
       title = "Purchase Production App",
+
+      # adding NOAA logo
+      tags$head(
+        tags$script(HTML(
+          '$(document).ready(function() {
+        $(".navbar .container-fluid")
+          .css({"position": "relative"})
+          .append("<img id=\'myImage\' src=\'noaa_header.png\'>");
+      });'
+        )),
+        tags$style(HTML(
+          '
+      .navbar {
+        min-height: 56px !important;
+      }
+
+      #myImage {
+        position: absolute;
+        right: 10px;
+        top: -10px;
+        height: 75px;
+      }
+
+      @media (max-width: 992px) {
+        #myImage {
+          position: fixed;
+          right: 10%;
+          top: 0.5%;
+        }
+      }
+      '
+        ))
+      ),
+
       # calling css file for more styles
       tags$head(
         tags$link(rel = "stylesheet", type = "text/css", href = "styles.css")
@@ -27,6 +61,7 @@ shinyUI(
       # START "Explore the Data" tab
       nav_panel(
         "Explore the Data",
+
         # START sidebarLayout
         sidebarLayout(
           ########################### Top tabSet ######################################
@@ -36,7 +71,7 @@ shinyUI(
           sidebarPanel(
             # START tabsetPanel
             tabsetPanel(
-              # START summary tabPanel
+              # START "Summary" tabPanel
               tabPanel(
                 "Summary",
                 # Metric
@@ -55,7 +90,7 @@ shinyUI(
                 )
               ), # END Summary tabPanel
 
-              # START Product Type tabPanel
+              # START "Product Type" tabPanel
               tabPanel(
                 "By Product Type",
                 # Metric
@@ -93,7 +128,7 @@ shinyUI(
 
             # START tabsetPanel
             tabsetPanel(
-              # START Production Activities tabPanel
+              # START "Production Activities" tabPanel
               tabPanel(
                 "Production Activities",
                 checkboxGroupInput(
@@ -102,6 +137,7 @@ shinyUI(
                   choices = prodac_choices,
                   selected = "All production"
                 ),
+                # Other Species
                 dropdownButton(
                   inputId = "osDropdown",
                   label = "Other Species",
@@ -112,7 +148,7 @@ shinyUI(
                     choices = osps_choices
                   )
                 )
-              ), # END Production Activities tabPanel
+              ), # END "Production Activities" tabPanel
 
               # START Region tabPanel
               tabPanel(
@@ -132,7 +168,7 @@ shinyUI(
                 )
               ), # END Region tabPanel
 
-              # START Processor size/type tabPanel
+              # START "Processor Size/Type" tabPanel
               tabPanel(
                 "Processor Size/Type",
                 checkboxGroupInput(
@@ -141,8 +177,8 @@ shinyUI(
                   choices = size_choices,
                   selected = "Small"
                 ),
+                # Production Actives (Processor type/size)
                 radioButtons(
-                  # Production Actives (Processor type/size)
                   inputId = "pracs2Input",
                   label = "Production activities",
                   choices = pracs_choices,
@@ -153,7 +189,7 @@ shinyUI(
               type = c("tabs")
             ), # END tabsetPanel
 
-            # Download data button
+            # downloadButton
             downloadButton(
               "downloadData",
               "Download",
@@ -166,14 +202,12 @@ shinyUI(
           # START mainPanel
           mainPanel(
             style = page_height,
-            # START tabsetPanel
+            # START navset_card_pill
             # tabsetPanel(
             navset_card_pill(
               height = 880, # height of the card that holds the plots
               full_screen = TRUE,
-              # type = "tabs",
-              # START plot tabPanel
-              # tabPanel(
+              # START "Plot" nav_panel
               nav_panel(
                 "Plot",
                 conditionalPanel(
@@ -184,21 +218,20 @@ shinyUI(
                   condition = "input.tab_top == 'By Product Type'",
                   plotOutput("productplot")
                 )
-              ), # END plot tabPanel
+              ), # END Plot nav_panel
 
-              # START table tabPanel
-              # tabPanel(
+              # START "Table" nav_panel
               nav_panel(
                 height = 880,
                 "Table",
                 DT::dataTableOutput("table")
-              ) # END  table tabPanel
-            ) # END tabsetPanel
+              ) # END  "Table" nav_panel
+            ) # END navset_card_pill
           ) # END mainPanel
         ) # END sidebarLayout
       ), # END "Explore the Data" tab
 
-      # tabPanel 2
+      # START "Information" nav_panel
       nav_panel(
         "Information Page",
         style = page_height,
@@ -208,15 +241,15 @@ shinyUI(
           column(10, includeMarkdown("text/info.md")),
           column(1)
         ),
-      ), #END tabPanel 2
+      ), # END "Information" nav_panel
 
-      #tabPanel 3
+      # START "Bulletin Board" nav_panel
       nav_panel(
         "Bulletin Board",
         style = page_height,
-      ), # END tabPanel 3
+      ), # END "Bulletin Board" nav_panel
 
-      #tabPanel 4
+      # START "Contact" Us nav_panel
       nav_panel(
         "Contact Us",
         style = page_height,
@@ -225,8 +258,8 @@ shinyUI(
           column(1),
           column(10, includeMarkdown("text/contact.md")),
           column(1)
-        ), # END intro text fluidRow
-      ), # END tabPanel 4
+        )
+      ), # "Contact" Us nav_panel
 
       footer = footer() # END footer
     ) # END page_navbar
